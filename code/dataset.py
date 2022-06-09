314a0191cccc690f3cfee8b73eb1c9a30181214c
Conanbranch <richard.branch@queensu.ca>
2022-06-09 12:04:17 -0400

    Update dataset.py

--- code/dataset.py
+++ code/dataset.py
@@ -4,18 +4,24 @@ from transformers import AutoTokenizer
 
 class MarkdownDataset(Dataset):
 
-    def __init__(self, df, model_name_or_path, total_max_len, md_max_len, fts):
+    def __init__(self, df, model_name_or_path, total_max_len, md_max_len, fts, vbl_code):
         super().__init__()
         self.df = df.reset_index(drop=True)
         self.md_max_len = md_max_len
         self.total_max_len = total_max_len  # maxlen allowed by model config
         self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
         self.fts = fts
+        self.vbl_code = vbl_code
 
     def __getitem__(self, index):
         row = self.df.iloc[index]
         sample_size = self.fts[row.id]["sample_size"]
-        code_max_length = int((self.total_max_len - self.md_max_len)/sample_size)
+        num_samples = self.fts[row.id]["num_samples"]
+        
+        if self.vbl_code == True:
+            code_max_length = int((self.total_max_len - self.md_max_len)/num_samples)
+        else:
+            code_max_length = int((self.total_max_len - self.md_max_len)/sample_size)
 
         inputs = self.tokenizer.encode_plus(
             row.source,
@@ -33,7 +39,6 @@ class MarkdownDataset(Dataset):
             max_length=code_max_length + 1,
             padding="max_length",
             truncation=True
-        )
         
         ids = inputs['input_ids']
         for x in code_inputs['input_ids']:
