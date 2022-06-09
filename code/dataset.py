cc8231a8acb58638c6c5675a8dc33ab99a443a0e
Conanbranch <richard.branch@queensu.ca>
2022-06-09 19:03:04 -0400

    bug fix

--- code/dataset.py
+++ code/dataset.py
@@ -33,6 +33,7 @@ class MarkdownDataset(Dataset):
             max_length=code_max_length + 1,
             padding="max_length",
             truncation=True
+        )
         
         ids = inputs['input_ids']
         for x in code_inputs['input_ids']:
