40c41da090b8dfe716ab4048cfe53ab1022b2bfd
Conanbranch <richard.branch@queensu.ca>
2022-06-13 10:43:48 -0400

    Update model.py

--- code/model.py
+++ code/model.py
@@ -5,20 +5,16 @@ from transformers import AutoModel, AutoTokenizer, AdamW, get_linear_schedule_wi
 
 
 class MarkdownModel(nn.Module):
-    def __init__(self, model_path, re_init = False, reinit_n_layers = 1):
+    def __init__(self, model_path, re_init = False, reinit_n_layers = 0):
         super(MarkdownModel, self).__init__()
         self.model = AutoModel.from_pretrained(model_path)
-        #self.top = nn.Linear(769, 1)
         self.top = nn.Linear(768, 1)
         if re_init == True:
             self.reinit_n_layers = reinit_n_layers
             for n in range(1, self.reinit_n_layers + 1):
                 self.model.encoder.layer[-n].apply(self.model._init_weights)
-
-    #def forward(self, ids, mask, fts):
+                
     def forward(self, ids, mask):
         x = self.model(ids, mask)[0]
-        #x = torch.cat((x[:, 0, :], fts), 1)
-        #x = self.top(x)
         x = self.top(x[:, 0, :])
         return x
