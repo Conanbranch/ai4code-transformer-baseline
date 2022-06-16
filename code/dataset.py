debf0c815a71c00ad0cb02813c18a93488757304
Conanbranch <richard.branch@queensu.ca>
2022-06-16 12:55:35 -0400

    Update dataset.py

--- code/dataset.py
+++ code/dataset.py
@@ -4,13 +4,15 @@ from transformers import AutoTokenizer
 
 class MarkdownDataset(Dataset):
 
-    def __init__(self, df, model_name_or_path, total_max_len, md_max_len, fts):
+    def __init__(self, df, model_name_or_path, total_max_len, md_max_len, fts, sep_token = True, eos_token = False):
         super().__init__()
         self.df = df.reset_index(drop=True)
         self.md_max_len = md_max_len
         self.total_max_len = total_max_len  # maxlen allowed by model config
         self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
         self.fts = fts
+        self.sep_token = sep_token
+        self.eos_token = eos_token
 
     def __getitem__(self, index):
         row = self.df.iloc[index]
@@ -37,16 +39,26 @@ class MarkdownDataset(Dataset):
         
         ids = inputs['input_ids']
         for x in code_inputs['input_ids']:
-            ids.extend(x[1:])
+            if self.sep_token == True:
+                ids.extend(x[1:])
+            else:
+                ids.extend(x[1:1])
         ids = ids[:self.total_max_len]
+        #if self.eos_token == True:
+        #    ids[self.total_max_len] = [EOS] 
         if len(ids) != self.total_max_len:
             ids = ids + [self.tokenizer.pad_token_id, ] * (self.total_max_len - len(ids))
         ids = torch.LongTensor(ids)
 
         mask = inputs['attention_mask']
         for x in code_inputs['attention_mask']:
-            mask.extend(x[1:])
+            if self.sep_token == True:
+                mask.extend(x[1:])
+            else:
+                ids.extend(x[1:1]) 
         mask = mask[:self.total_max_len]
+        #if self.eos_token == True:
+        #   ids[self.total_max_len] = [EOS] 
         if len(mask) != self.total_max_len:
             mask = mask + [self.tokenizer.pad_token_id, ] * (self.total_max_len - len(mask))
         mask = torch.LongTensor(mask)
