8b803276dfaee95c2356976403343f3d67cdbe80
Conanbranch <richard.branch@queensu.ca>
2022-06-28 13:02:44 -0400

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -2,6 +2,31 @@ from io import StringIO
 import tokenize
 import re
 
+fails = []
+
+def return_default_value_if_failed(default_value):
+    def decorator(func):
+        def inner(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                fails.append((func, (args, kwargs), e))
+                return default_value
+        return inner
+    return decorator
+
+def return_unmodified_value_if_failed():
+    def decorator(func):
+        def inner(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                fails.append((func, (args, kwargs), e))
+                return args
+        return inner
+    return decorator
+
+#@return_unmodified_value_if_failed()
 def preprocess_text(document):
     #document = re.sub(r"[^a-zA-Z0-9]+", ' ', str(document)) # Remove all the special characters except 
     #document = re.sub(r"[^a-zA-Z0-9#]+", ' ', str(document)) # Remove all the special characters except # which indicates heading level
@@ -11,11 +36,13 @@ def preprocess_text(document):
     #document = document.lower() # Converting to Lowercase
     #document = document.lstrip()
     return document
-    
+
+#@return_unmodified_value_if_failed()
 def preprocess_markdown(df):
     return [preprocess_text(message) for message in df.source]
 
 # Source: https://stackoverflow.com/questions/1769332/script-to-remove-python-comments-docstrings
+@return_unmodified_value_if_failed()
 def remove_comments_and_docstrings(source):
     """
     Returns 'source' minus comments and docstrings.
@@ -72,6 +99,7 @@ def remove_comments_and_docstrings(source):
         last_lineno = end_line
     return out
 
+#@return_unmodified_value_if_failed()
 def clean_code(cell):
     cell = str(remove_comments_and_docstrings(cell))
     cell = str(cell).replace("\\n", "\n")
