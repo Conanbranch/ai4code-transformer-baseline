addb2d3ee1d0ae1df9c1b3eadcde93f810ceea75
Conanbranch <richard.branch@queensu.ca>
2022-06-30 12:30:51 -0400

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -131,7 +131,8 @@ def clean_markdown(document):
     #document = re.sub('https?://\S+|www\.\S+', '', str(document)) # remove links
     document = re.sub(r"[^a-zA-Z0-9]+", ' ', str(document)) # remove all the special characters 
     #document = re.sub(r"[^a-zA-Z0-9#]+", ' ', str(document)) # remove all the special characters except # which indicates heading level
-    #document = re.sub('\w*\d\w*', '', str(document)) # remove numbers
+    #document = re.sub('\w*\d\w*', ' ', str(document)) # remove words with numbers
+    #document = re.sub(r'[0-9]+', '', str(document)) # remove numbers from words and on their own
     #document = re.sub(r"\b[a-zA-Z]\b", ' ', str(document)) # remove all single characters
     #document = re.sub(r'\s+', ' ', str(document), flags=re.I) # substitute multiple spaces with single space   
     #document = re.sub(r'^b\s+', '', str(document)) # removing prefixed 'b'
