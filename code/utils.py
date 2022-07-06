68a4ff6132c01d60a831c31a2af98d5be0794a37
Conanbranch <richard.branch@queensu.ca>
2022-07-06 08:42:40 -0400

    Update utils.py

--- code/utils.py
+++ code/utils.py
@@ -133,11 +133,11 @@ def clean_markdown(document):
 
 #@return_unmodified_value_if_failed()
 def clean_code(cell):
-    cell = remove_comments_and_docstrings(str(cell)) #remove comments
+    #cell = remove_comments_and_docstrings(str(cell)) #remove comments
     cell = str(cell).replace("\\n", "\n") # fix newlines
-    cell = str(cell).replace("\n", " ") # remove newlines
-    cell = remove_special_tokens(str(cell)) # remove any special tokens
-    cell = re.sub(r' +', ' ', str(cell)) # remove multiple spaces
-    cell = str(cell).strip() # remove leading and following spaces
-    cell = str(cell).lower() # converting to Lowercase
+    #cell = str(cell).replace("\n", " ") # remove newlines
+    #cell = remove_special_tokens(str(cell)) # remove any special tokens
+    #cell = re.sub(r' +', ' ', str(cell)) # remove multiple spaces
+    #cell = str(cell).strip() # remove leading and following spaces
+    #cell = str(cell).lower() # converting to Lowercase
     return cell
