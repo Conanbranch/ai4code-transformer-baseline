e87c5efc2ec50e1ffd074f845fa1cbed8a0bc767
Conanbranch <richard.branch@queensu.ca>
2022-07-06 16:43:48 -0400

    Update train.py

--- code/train.py
+++ code/train.py
@@ -261,6 +261,7 @@ if args.resume_train != True:
     best_loss = 0
     best_initial_state = 0
     for i in range(0,10):
+        print("restart: " + i)
         model = MarkdownModel(args.model_name_or_path, args.re_init, args.reinit_n_layers)
         model = model.cuda()
         loss, state = multiple_restart_train(model, train_loader, val_loader, epochs=args.epochs)
