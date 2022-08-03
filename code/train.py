6f32b1f3e65785c0db19ce25a03501c04ba21a4e
Conanbranch <richard.branch@queensu.ca>
2022-08-03 11:23:03 -0400

    Update train.py

--- code/train.py
+++ code/train.py
@@ -168,7 +168,7 @@ def train(model, train_loader, val_loader, epochs):
                 return model, y_pred
             
         if args.early_stop:
-            if e == early_stop_num:
+            if e == args.early_stop_num:
                 return model, y_pred        
         
         model.train()
